b20617a6295937236107025d30d22f5f88687de1
ngmisl <98217124+ngmisl@users.noreply.github.com>
2022-08-12 22:57:37 +0200

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="bg-dark text-center text-lg-start">
             <div className="text-center p-3">
-                © 2022 Copyright
+                © 2022 Copyright{' '}
                 <a className="text-white" href="https://mintworldgame.com">
                     MintWorldGame.com{' '}
                 </a>
